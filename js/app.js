a495462438c90cd0401d0ce7b83599926fb5c2ad
Geo2004 <46329778+geo2004@users.noreply.github.com>
2022-09-06 16:19:46 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const m_color = new L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={
 });
 
 //ArcGIS Satellite
-const m_mono = new L.esri.tiledMapLayer('https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer', {
+const m_mono = new L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution:
         "Maptiles by <a href='http://arcgis.com/' target='_blank'>ESRI ARCGIS</a>, under CC BY. Data by <a href='https://esri.com' target='_blank'>ESRI</a> contributors, ",
 });
